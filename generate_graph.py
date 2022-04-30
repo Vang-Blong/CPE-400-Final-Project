d52b35446ec5127a79f2b43ab5d322f6f02cc2f0
Blong Vang <mikey.b.vang@gmail.com>
2022-04-29 23:14:54 -0700

    initial

--- generate_graph.py
+++ generate_graph.py
@@ -2,9 +2,43 @@
 #CPE400 Final Project Spring 2022
 #Generates random graph file
 
-from . import user
-from . import dir
-from random import random
+from sys import path
+path.append(".")
+from graph import Graph  #imports graph class
+from random import random,randint  #for random graph on every instance
 
-num_nodes = random() * 10
+def generate_graph():
+    
+    # designates and generates graph with random # of nodes
+    num_nodes = randint(8,12)
+    graph = Graph(num_nodes)
 
+    #tracker to ensure every node has an edge
+    tracker= []
+
+    for node in range(1, num_nodes):
+        tracker.append(node)
+
+    while len(tracker) > 0:
+        #picks two different edges
+        edge1 = randint(1,num_nodes - 2)
+        edge2 = randint(edge1 + 1, num_nodes)
+        #generates unique chance to fail
+        fail_chance = randint(0,10)
+        
+        #ensures every node gets an edge
+        if edge1 in tracker:
+            tracker.remove(edge1)
+        if edge2 in tracker:
+            tracker.remove(edge2)
+
+        #populates graph with fixed values
+        graph.add_edge(edge1, edge2, fail_chance)
+
+    graph.bellman_ford(2)
+    #removes redundant graph objects
+    
+
+generate_graph()
+#def __init__():
+#    generate_graph()
--- graph.py
+++ graph.py
@@ -10,8 +10,27 @@ class Graph:
         self.graph = []
     
     #connects nodes and adds edge properties
-    #fail chance = random value 1-100 of node to fail
-    #score = defaults to 50, will dynamically attempt to calculate fail chance over time
-    def add_edge(self, node1, node2, fail_chance, score=50):
+    #fail chance = random value 1-10 (each is 10%),of node to fail
+    #score = defaults to 0, will dynamically attempt to calculate fail chance over time
+    def add_edge(self, node1, node2, fail_chance, score=0):
         self.graph.append([node1,node2, fail_chance, score])
 
+    
+    def print_routing_table(self, distance):
+        print("Node  Distance")
+        for i in range(self.nodes):
+            print("{0}\t\t{1}".format(i,distance[i]))
+
+    def bellman_ford(self, origin):
+
+        #generate initial routing table, setting all values to infinity and distance from self to 0
+        distance = [float("infinity")] * self.nodes
+        distance[origin] = 0
+
+        #establishes reliability for all nodes in the graph using a score
+        for _ in range(self.nodes - 1):
+            for node1, node2, fail_chance, score in self.graph:
+                if distance[node1] != float("infinity") and distance[node1] + score < distance[node2]:
+                    distance[node2] = distance[1] + score
+        
+        self.print_routing_table(distance)
